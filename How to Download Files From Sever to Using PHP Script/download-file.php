acb0ed645dd3e690f18e171d76d1cc0140d71c75
skptricks <sumit.pradhan96@gmail.com>
2018-04-14 15:01:11 +0530

    Add files via upload

---
+++ How to Download Files From Sever to Using PHP Script/download-file.php
@@ -0,0 +1,16 @@
+<?php
+//Image file at sever loaction to download
+$file = 'skp.png';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+?>
